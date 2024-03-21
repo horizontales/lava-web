a3393a4786f4d87dbb5efcb5e4169cdbf365fcfc
horizontales <98145028+horizontales@users.noreply.github.com>
2024-03-21 23:20:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-6ca45a0a-93d6-4b48-a0b2-03f35c8cf3ac/)
 
 
 echo "$curl_output" >> Update.tsx
